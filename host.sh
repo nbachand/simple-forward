c8bc4c6f23fab5c0b5880ca49f7d78cbb80c503b
Nicholas <ngbachand@gmail.com>
2023-04-29 15:29:01 -0700

    changing default env

--- host.sh
+++ host.sh
@@ -4,8 +4,8 @@
 port=52938
 
 source ~/.bashrc
-read -p "Enter conda env [cascade_env]: " env
-env=${env:-cascade_env}
+read -p "Enter conda env [pycascade]: " env
+env=${env:-pycascade}
 echo $env
 conda activate $env
 
